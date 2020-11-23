10f248a8849f3a4c5517ada5e6ffae5b234a8fad
Sergey Biryukov <2700036@mail.ru>
2020-11-23 23:32:52 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { motion } from 'framer-motion';
 
 export default function App() {
   const {isAnswerCorrect, nextQuestion, category, question} = useTrivia();
-
+  
   return (
     <div className="app">
       <h1>Trivia</h1>
